2ee3d4b8349a814bc25d25f6bd97d407a6b79829
Paul John B. Parreno <paul.john.parreno@gmail.com>
2021-01-26 16:14:19 +0800

    commiting changes to MainActivity by applying view binding

--- app/src/main/java/com/pparreno/kotlintrntasklist/ui/main/MainActivity.kt
+++ app/src/main/java/com/pparreno/kotlintrntasklist/ui/main/MainActivity.kt
@@ -2,25 +2,27 @@ package com.pparreno.kotlintrntasklist.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
-import androidx.appcompat.app.AppCompatActivity
-import com.pparreno.kotlintrntasklist.R
+import com.pparreno.kotlintrntasklist.databinding.ActivityMainBinding
 import com.pparreno.kotlintrntasklist.ui.compose.ComposeNoteActivity
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        val viewPager: ViewPager = binding.viewPager
         viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = findViewById(R.id.tabs)
+        val tabs: TabLayout = binding.tabs
         tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-
+        val fab: FloatingActionButton = binding.fab
         fab.setOnClickListener { view ->
             val intent = Intent(this@MainActivity, ComposeNoteActivity::class.java)
             startActivity(intent)
