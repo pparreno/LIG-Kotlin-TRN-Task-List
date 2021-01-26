83316c262ed1e7e7a4d9f9d5591df5ea5af23f2f
Paul John B. Parreno <paul.john.parreno@gmail.com>
2021-01-26 10:51:40 +0800

    commiting changes to app with necessary tabs for the APP UI

--- app/src/main/java/com/pparreno/kotlintrntasklist/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/pparreno/kotlintrntasklist/ui/main/SectionsPagerAdapter.kt
@@ -7,8 +7,9 @@ import androidx.fragment.app.FragmentPagerAdapter
 import com.pparreno.kotlintrntasklist.R
 
 private val TAB_TITLES = arrayOf(
-        R.string.tab_text_1,
-        R.string.tab_text_2
+        R.string.all,
+        R.string.favorites,
+        R.string.important
 )
 
 /**
@@ -30,6 +31,6 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
 
     override fun getCount(): Int {
         // Show 2 total pages.
-        return 2
+        return 3
     }
 }
