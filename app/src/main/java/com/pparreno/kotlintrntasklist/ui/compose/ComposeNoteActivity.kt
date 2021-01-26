bc5f9438aeb47589604d95bef8f6a37f81dde858
Paul John B. Parreno <paul.john.parreno@gmail.com>
2021-01-26 15:09:10 +0800

    commiting new menu for compose note screen

--- app/src/main/java/com/pparreno/kotlintrntasklist/ui/compose/ComposeNoteActivity.kt
+++ app/src/main/java/com/pparreno/kotlintrntasklist/ui/compose/ComposeNoteActivity.kt
@@ -2,11 +2,18 @@ package com.pparreno.kotlintrntasklist.ui.compose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.pparreno.kotlintrntasklist.R
 
 class ComposeNoteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_compose_note)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.compose_note_menu, menu)
+        return true
     }
 }
