7d88304d3304486fe953b95ee2e13f4b374550d4
Paul John B. Parreno <paul.john.parreno@gmail.com>
2021-01-27 14:49:52 +0800

    using custom Application class for use with Hilt

---
+++ app/src/main/java/com/pparreno/kotlintrntasklist/application/MyAppApplication.kt
@@ -0,0 +1,8 @@
+package com.pparreno.kotlintrntasklist.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyAppApplication : Application() {
+}
