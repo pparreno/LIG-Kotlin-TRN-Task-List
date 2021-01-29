e8a83d295a99b52daac27217f93e94f0f32c3330
Paul John B. Parreno <paul.john.parreno@gmail.com>
2021-01-29 09:19:36 +0800

    commiting changes to allow some viusal separation between note items in Notes List

--- app/src/main/java/com/pparreno/kotlintrntasklist/ui/notes/list/NotesListAdapter.kt
+++ app/src/main/java/com/pparreno/kotlintrntasklist/ui/notes/list/NotesListAdapter.kt
@@ -1,12 +1,15 @@
 package com.pparreno.kotlintrntasklist.ui.notes.list
 
 import android.app.Activity
+import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.pparreno.kotlintrntasklist.R
 import com.pparreno.kotlintrntasklist.databinding.NoteRecyclerItemBinding
 import com.pparreno.kotlintrntasklist.room.data.Note
 import org.ocpsoft.prettytime.PrettyTime
@@ -30,6 +33,12 @@ class NotesListAdapter(var activity: Activity, var list: List<Note>) : RecyclerV
         holder.contentText.text = note.content
         val p = PrettyTime()
         holder.dateText.text = p.format(note.dateCreated)
+        if (position % 2 == 0)
+        {
+            holder.itemView.rootView.background = ColorDrawable(ContextCompat.getColor(activity, R.color.white))
+        } else {
+            holder.itemView.rootView.background = ColorDrawable(ContextCompat.getColor(activity, R.color.light_gray))
+        }
     }
 
     override fun getItemCount(): Int {
