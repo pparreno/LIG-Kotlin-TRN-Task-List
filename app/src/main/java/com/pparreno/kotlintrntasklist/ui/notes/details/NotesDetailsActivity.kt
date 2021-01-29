3acb1d54c83d3aba237675f5e73e7e90f80d948a
Paul John B. Parreno <paul.john.parreno@gmail.com>
2021-01-29 09:48:56 +0800

    commiting binding of values in NotesDetailsActivity

--- app/src/main/java/com/pparreno/kotlintrntasklist/ui/notes/details/NotesDetailsActivity.kt
+++ app/src/main/java/com/pparreno/kotlintrntasklist/ui/notes/details/NotesDetailsActivity.kt
@@ -3,6 +3,8 @@ package com.pparreno.kotlintrntasklist.ui.notes.details
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.pparreno.kotlintrntasklist.databinding.ActivityNotesDetailsBinding
+import com.pparreno.kotlintrntasklist.room.data.Note
+import org.ocpsoft.prettytime.PrettyTime
 
 class NotesDetailsActivity : AppCompatActivity() {
     lateinit var viewBinding: ActivityNotesDetailsBinding
@@ -11,6 +13,16 @@ class NotesDetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityNotesDetailsBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+        val note = intent.getParcelableExtra<Note>(KEY_EXTRA_NOTE)
+        note?.let { setViewValues(it) }
+    }
+
+    private fun setViewValues(note : Note) {
+        viewBinding.titleText.text = note.title
+        viewBinding.contentText.text = note.content
+        val p = PrettyTime()
+        viewBinding.dateText.text = p.format(note.dateCreated)
     }
 
     companion object {
